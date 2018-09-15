a184f2f80d4e6004fa5aab25727d550562346a6a
nebiyat-t <nebiyatzelealem@gmail.com>
2018-09-15 14:14:49 -0400

    Changed HTML file to fit my resume requirements and updated CSS file

--- main.js
+++ main.js
@@ -10,9 +10,26 @@ console.log("x = " + x);
 
 let city = "Addis Ababa";
 
-console.log("city = " + city);
+console.log("city = " + city)
 
 city = "Washington D.C";
 
-console.log("city = " + city);
+console.log("city = " + city)
+
+let speed = 50;
+
+if (speed<30) {
+    console.log("no ticket")
+}
+
+else if(speed<=40) {
+    console.log("50 dollar ticket")
+}
+
+else {
+    console.log("100 dollar ticket")
+}
+console.log("getting to the bottom of page")
+let pro = confirm("Welcome to my page, Enjoy my resume!");
+console.log(pro)
 
