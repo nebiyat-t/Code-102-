49de46e1be60a10885990afcbae357f5bf915734
nebiyat-t <nebiyatzelealem@gmail.com>
2018-09-15 11:31:07 -0400

    Created JavaScript file

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+let x = "street";
+console.log("x = " + x);
+
+x = "phone";
+console.log("x = " + x);
+
+x = "wire";
+console.log("x = " + x);
+
+let city = "Addis Ababa";
+
+console.log("city = " + city);
+
+city = "Washington D.C";
+
+console.log("city = " + city);
+
